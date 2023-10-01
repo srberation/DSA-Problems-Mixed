6816324c620a16ebecbddd7197e30aabfbe3d463
sbisht3 <36527738+srberation@users.noreply.github.com>
2023-10-01 16:20:52 +0530

    Time: 33 ms (85.99%), Space: 55.2 MB (26%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,50 +1,41 @@
 class Trie {
+
     private boolean isEnd;
     private Trie[] children;
 
     public Trie() {
-        // Trie structure will have a flag for finding end of the word and children array of 26 size 
         isEnd = false;
         children = new Trie[26];
-        for (int i = 0; i < 26; i++) {
-            children[i] = null;
-        }
+        for(int i=0; i<26; i++) children[i] = null;
     }
 
     public void insert(String word) {
-        // By assigning this to the curr variable, you are creating a reference to the current Trie node. This is commonly done to traverse or manipulate the Trie data structure from the current node
-        Trie curr = this;
-        for (char ch : word.toCharArray()) {
-            int index = ch - 'a';  // get index of the curr character
-            if (curr.children[index] == null) {
-                curr.children[index] = new Trie();
-            }
-            curr = curr.children[index];
-        }
+        Trie curr = this;  // will work as root
+        for(char ch: word.toCharArray()){
+            int idx = ch-'a';
+            if(curr.children[idx] == null) curr.children[idx] = new Trie();
+            curr = curr.children[idx];
+        }  
         curr.isEnd = true;
     }
 
     public boolean search(String word) {
         Trie curr = this;
-        for (char ch : word.toCharArray()) {
-            int index = ch - 'a';
-            if (curr.children[index] == null) {
-                return false;
-            }
-            curr = curr.children[index];
-        }
-        return curr.isEnd;
+        for(char ch: word.toCharArray()){
+            int idx = ch-'a';
+            if(curr.children[idx] == null) return false;
+            curr = curr.children[idx];
+        }  
+        return curr.isEnd;       
     }
 
     public boolean startsWith(String prefix) {
-        Trie curr = this;
-        for (char ch : prefix.toCharArray()) {
-            int index = ch - 'a';
-            if (curr.children[index] == null) {
-                return false;
-            }
-            curr = curr.children[index];
-        }
-        return true;
+         Trie curr = this;
+        for(char ch: prefix.toCharArray()){
+            int idx = ch-'a';
+            if(curr.children[idx] == null) return false;
+            curr = curr.children[idx];
+        }  
+        return true;     
     }
 }
