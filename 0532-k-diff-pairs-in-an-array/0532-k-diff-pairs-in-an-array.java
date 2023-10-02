951c3533f66e4dd4c4ad1b20bf4191667c390173
sbisht3 <36527738+srberation@users.noreply.github.com>
2023-10-02 15:32:55 +0530

    Time: 10 ms (66.99%), Space: 44.7 MB (17.05%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,21 +1,36 @@
-class Solution {
-    public int findPairs(int[] nums, int k) {
-        int count = 0;
-        Arrays.sort(nums);
-        int l=0,r=1;
-        while(l< nums.length && r < nums.length ){
-            if(nums[r]-nums[l] == k ){
-                count++;
-                l++;
-                r++;
-                while(r<nums.length && nums[r]==nums[r-1]) r++;
-            }
-            else if(nums[r]-nums[l] > k) {
-                l++;
-                if(r-l == 0) r++;
-            }
-            else r++;
-        }
-        return count;
-    }
-}
+// class Solution {
+//     public int findPairs(int[] nums, int k) {
+//         int count = 0;
+//         Arrays.sort(nums);
+//         int l=0,r=1;
+//         while(l< nums.length && r < nums.length ){
+//             if(nums[r]-nums[l] == k ){
+//                 count++;
+//                 l++;
+//                 r++;
+//                 while(r<nums.length && nums[r]==nums[r-1]) r++;
+//             }
+//             else if(nums[r]-nums[l] > k) {
+//                 l++;
+//                 if(r-l == 0) r++;
+//             }
+//             else r++;
+//         }
+//         return count;
+//     }
+// }
+ // O(n) Time Solution
+
+ class Solution {
+ 		public int findPairs(int[] nums, int k) {
+ 			Map<Integer, Integer> map = new HashMap();
+ 			for (int num : nums)
+ 				map.put(num, map.getOrDefault(num, 0) + 1);
+
+ 			int result = 0;
+ 			for (int i : map.keySet())
+ 				if (k > 0 && map.containsKey(i + k) || k == 0 && map.get(i) > 1)
+ 					result++;
+ 			return result;
+ 		}
+ 	}
